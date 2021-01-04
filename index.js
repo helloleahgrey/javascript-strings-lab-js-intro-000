b9b9677be0d825559f3b9137417fc9c662e5a07a
voluble-request-f2d5 <leahmarizan@gmail.com>
2021-01-04 05:34:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ var greeting = "Hello, everybody!";
 
 var specialGuest = 'Hello, Neil deGrasse Tyson!'
 
-var greetSpecialGuest = "" + specialguest + "!";
+var greetSpecialGuest = `Hello, ${specialGuest}!`;
 
 var topic = 'Let\'s talk about space.'
 
-var conversation = `'Let\'s talk about space.'${topic}`;
+var conversation = `Let's talk about ${topic}.`;
